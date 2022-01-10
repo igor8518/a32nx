8268091c01923595c520af911e9b33859c12fbc8
Benjamin Dupont <benjozorkfr@gmail.com>
2022-01-09 23:26:26 -0500

    start idle path, better altitudeStep

--- src/fmgc/src/guidance/vnav/descent/DescentPathBuilder.ts
+++ src/fmgc/src/guidance/vnav/descent/DescentPathBuilder.ts
@@ -1,10 +1,26 @@
 import { TheoreticalDescentPathCharacteristics } from '@fmgc/guidance/vnav/descent/TheoreticalDescentPath';
 import { NavGeometryProfile, VerticalCheckpointReason } from '@fmgc/guidance/vnav/profile/NavGeometryProfile';
+import { BaseGeometryProfile } from '@fmgc/guidance/vnav/profile/BaseGeometryProfile';
+import { SpeedProfile } from '@fmgc/guidance/vnav/climb/SpeedProfile';
+import { Predictions, StepResults } from '@fmgc/guidance/vnav/Predictions';
+import { FlapConf } from '@fmgc/guidance/vnav/common';
+import { AtmosphericConditions } from '@fmgc/guidance/vnav/AtmosphericConditions';
+import { VerticalProfileComputationParametersObserver } from '@fmgc/guidance/vnav/VerticalProfileComputationParameters';
+import { ClimbPathBuilder } from '@fmgc/guidance/vnav/climb/ClimbPathBuilder';
 
 export class DescentPathBuilder {
-    computeDescentPath(profile: NavGeometryProfile): TheoreticalDescentPathCharacteristics {
-        const TEMP_FUEL_BURN = 2000;
+    private atmosphericConditions: AtmosphericConditions = new AtmosphericConditions();
 
+    constructor(
+        private computationParametersObserver: VerticalProfileComputationParametersObserver,
+    ) {
+    }
+
+    update() {
+        this.atmosphericConditions.update();
+    }
+
+    computeDescentPath(profile: NavGeometryProfile, speedProfile: SpeedProfile): TheoreticalDescentPathCharacteristics {
         const decelCheckpoint = profile.checkpoints.find((checkpoint) => checkpoint.reason === VerticalCheckpointReason.Decel);
 
         if (!decelCheckpoint) {
@@ -20,42 +36,98 @@ export class DescentPathBuilder {
             console.log(verticalDistance);
         }
 
-        const tod = decelCheckpoint.distanceFromStart - (verticalDistance / Math.tan((fpa * Math.PI) / 180)) * 0.000164579;
+        const todEstimate = decelCheckpoint.distanceFromStart - (verticalDistance / Math.tan((fpa * Math.PI) / 180)) * 0.000164579;
 
-        if (DEBUG) {
-            console.log(`[FMS/VNAV] T/D: ${tod.toFixed(1)}nm`);
+        const tocCheckpoint = profile.findVerticalCheckpoint(VerticalCheckpointReason.TopOfClimb);
+
+        if (tocCheckpoint) {
+            // Estimate ToD checkpoint
+            const todEstimateDistanceFromStart = Math.min(tocCheckpoint.distanceFromStart, todEstimate);
+
+            profile.checkpoints.push({
+                reason: VerticalCheckpointReason.TopOfDescent,
+                distanceFromStart: todEstimateDistanceFromStart,
+                secondsFromPresent: profile.interpolateTimeAtDistance(todEstimateDistanceFromStart),
+                altitude: tocCheckpoint.altitude,
+                remainingFuelOnBoard: profile.interpolateFuelAtDistance(todEstimateDistanceFromStart),
+                speed: tocCheckpoint.speed,
+            });
+
+            const todEstimateCheckpoint = profile.findVerticalCheckpoint(VerticalCheckpointReason.TopOfDescent);
+
+            this.buildIdlePath(profile, speedProfile, todEstimateCheckpoint.altitude, decelCheckpoint.altitude);
+
+            profile.sortCheckpoints();
+
+            const lastIdlePathCheckpoint = profile.findLastVerticalCheckpoint(VerticalCheckpointReason.IdlePathAtmosphericConditions);
+
+            // Check that the idle path ends before our reference point (at the moment, always DECEL)
+            if (lastIdlePathCheckpoint.distanceFromStart > decelCheckpoint.distanceFromStart) {
+                // If so, do not do an idle path for now TODO insert a vertical discontinuity ?
+                profile.purgeVerticalCheckpoints(VerticalCheckpointReason.IdlePathAtmosphericConditions);
+            }
+
+            return {
+                tod: todEstimate,
+                fuelBurnedDuringDescent: tocCheckpoint.remainingFuelOnBoard - lastIdlePathCheckpoint.remainingFuelOnBoard,
+                remainingFuelOnBoardAtEndOfIdlePath: lastIdlePathCheckpoint.remainingFuelOnBoard,
+                remainingFuelOnBoardAtTopOfDescent: todEstimateCheckpoint.remainingFuelOnBoard,
+            };
         }
 
-        profile.checkpoints.push({
-            reason: VerticalCheckpointReason.TopOfDescent,
-            distanceFromStart: tod,
-            speed: 290,
-            remainingFuelOnBoard: decelCheckpoint.remainingFuelOnBoard + TEMP_FUEL_BURN,
-            altitude: cruiseAlt,
-        });
+        console.error('[FMS/VNAV](computeDescentPath) Cannot compute descent path without ToC');
 
-        return { tod, fuelBurnedDuringDescent: TEMP_FUEL_BURN, remainingFuelOnBoardAtTopOfDescent: decelCheckpoint.remainingFuelOnBoard + TEMP_FUEL_BURN };
+        return undefined;
+    }
 
-        //     const decelPointDistance = DecelPathBuilder.computeDecelPath(geometry);
-        //
-        //     const lastLegIndex = geometry.legs.size - 1;
-        //
-        //     // Find descent legs before decel point
-        //     let accumulatedDistance = 0;
-        //     let currentLegIdx;
-        //     let currentLeg;
-        //     for (currentLegIdx = lastLegIndex; accumulatedDistance < decelPointDistance; currentLegIdx--) {
-        //         currentLeg = geometry.legs.get(currentLegIdx);
-        //
-        //         accumulatedDistance += currentLeg.distance;
-        //     }
-        //     currentLegIdx--;
-        //
-        //     const geometricPath = GeomtricPathBuilder.buildGeometricPath(geometry, currentLegIdx);
-        //
-        //     console.log(geometricPath);
-        //
-        //     return { geometricPath };
-        // }
+    private buildIdlePath(profile: BaseGeometryProfile, speedProfile: SpeedProfile, startingAltitude: Feet, targetAltitude: Feet): void {
+        if (targetAltitude > startingAltitude) {
+            throw new Error('[FMS/VNAV/DescentPathBuilder] targetAltitude was greater than startingAltitude.');
+        }
+
+        for (let altitude = startingAltitude; altitude > targetAltitude; altitude = Math.max(altitude - 1500, targetAltitude)) {
+            const lastCheckpoint = profile.lastCheckpoint;
+
+            const speed = speedProfile.get(lastCheckpoint.distanceFromStart, altitude);
+
+            const targetAltitudeForSegment = Math.max(altitude - 1500, targetAltitude);
+            const remainingFuelOnBoard = lastCheckpoint.remainingFuelOnBoard;
+
+            const { distanceTraveled, fuelBurned, timeElapsed } = this.computeIdlePathSegmentPrediction(altitude, targetAltitudeForSegment, speed, remainingFuelOnBoard);
+
+            profile.checkpoints.push({
+                reason: VerticalCheckpointReason.IdlePathAtmosphericConditions,
+                distanceFromStart: lastCheckpoint.distanceFromStart + distanceTraveled,
+                secondsFromPresent: lastCheckpoint.secondsFromPresent + (timeElapsed * 60),
+                altitude: targetAltitudeForSegment,
+                remainingFuelOnBoard: remainingFuelOnBoard - fuelBurned,
+                speed: speedProfile.get(lastCheckpoint.distanceFromStart + distanceTraveled, targetAltitudeForSegment),
+            });
+        }
+    }
+
+    private computeIdlePathSegmentPrediction(startingAltitude: Feet, targetAltitude: Feet, climbSpeed: Knots, remainingFuelOnBoard: number): StepResults {
+        const { zeroFuelWeight, perfFactor, tropoPause } = this.computationParametersObserver.get();
+
+        const midwayAltitudeClimb = (startingAltitude + targetAltitude) / 2;
+        const machClimb = this.atmosphericConditions.computeMachFromCas(midwayAltitudeClimb, climbSpeed);
+
+        const predictedN1 = 26 + ((targetAltitude / midwayAltitudeClimb) * (30 - 26));
+
+        return Predictions.altitudeStep(
+            startingAltitude,
+            targetAltitude - startingAltitude,
+            climbSpeed,
+            machClimb,
+            predictedN1,
+            zeroFuelWeight * ClimbPathBuilder.TONS_TO_POUNDS,
+            remainingFuelOnBoard,
+            0,
+            this.atmosphericConditions.isaDeviation,
+            tropoPause,
+            false,
+            FlapConf.CLEAN,
+            perfFactor,
+        );
     }
 }
