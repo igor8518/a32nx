178ab15c2dbb2302c60d4f852ea3660f603b507b
Igor Milovidov <igor8518@mail.ru>
2022-01-21 13:53:46 +0700

    feat:SS FULL

--- src/instruments/src/EFB/Settings/sync.ts
+++ src/instruments/src/EFB/Settings/sync.ts
@@ -22,6 +22,8 @@ const settingsToSync: Map<string, SimVar> = new Map([
     ['ISIS_BARO_UNIT_INHG', ['L:A32NX_ISIS_BARO_UNIT_INHG', 'number', '0']],
     ['REALISTIC_TILLER_ENABLED', ['L:A32NX_REALISTIC_TILLER_ENABLED', 'number', '0']],
     ['HOME_COCKPIT_ENABLED', ['L:A32NX_HOME_COCKPIT_ENABLED', 'number', '0']],
+    ['AUTO_DELETE_DISCONTINUITY', ['L:A32NX_AUTO_DELETE_DISCONTINUITY', 'number', '1']],
+    ['AUTO_SID_STAR', ['L:A32NX_AUTO_SID_STAR', 'number', '1']],
 ]);
 
 export function readSettingsFromPersistentStorage() {
