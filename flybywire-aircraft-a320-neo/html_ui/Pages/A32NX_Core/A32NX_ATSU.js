748dbba1726db1ace15fbbfd0aa0dcfc2f4df4a3
Igor Milovidov <igor8518@mail.ru>
2022-01-21 13:39:31 +0700

    AI: Auto
    feat:AI FULL
    
    feat:AI2
    
    AI: mcdu refactoring fix
    
    AI: more fixes
    
    AI: More fixes
    
    AI: minor fix
    
    AI: fix vspeeds
    
    fix vspeeds
    
    Update A32NX_InitFlight.js
    
    AI
    
    AI

--- flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_ATSU.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/A32NX_Core/A32NX_ATSU.js
@@ -143,9 +143,11 @@ const insertUplink = (mcdu) => {
 
             await mcdu.tryUpdateAltDestination(alternateIcao);
 
+            SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_AOC", "Number", 1);
             setTimeout(async () => {
                 await uplinkRoute(mcdu);
                 mcdu.setScratchpadMessage(NXSystemMessages.aocActFplnUplink);
+                SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_AOC", "Number", 2);
             }, mcdu.getDelayRouteChange());
 
             if (mcdu.page.Current === mcdu.page.InitPageA) {
@@ -153,17 +155,20 @@ const insertUplink = (mcdu) => {
             }
         }
     });
+    SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_FLTNBR", "Number", 1);
     mcdu.updateFlightNo(callsign, (result) => {
         if (result) {
             if (mcdu.page.Current === mcdu.page.InitPageA) {
                 CDUInitPage.ShowPage1(mcdu);
             }
         }
+        SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_FLTNBR", "Number", 2);
     });
 
     /**
      * INIT PAGE DATA UPLINK
     */
+    SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_UPLINK", "Number", 1);
     setTimeout(() => {
         mcdu.setCruiseFlightLevelAndTemperature(cruiseAltitude);
         mcdu.tryUpdateCostIndex(costIndex);
@@ -171,6 +176,7 @@ const insertUplink = (mcdu) => {
         if (mcdu.page.Current === mcdu.page.InitPageA) {
             CDUInitPage.ShowPage1(mcdu);
         }
+        SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_UPLINK", "Number", 2);
     }, mcdu.getDelayHigh());
 };
 
@@ -1105,6 +1111,7 @@ const uplinkRoute = async (mcdu, coroute = false) => {
             }
         }*/
     }
+    await SimVar.SetSimVarValue("L:A32NX_INITFLIGHT_UPLINK", "Number", 3);
 };
 
 /**
