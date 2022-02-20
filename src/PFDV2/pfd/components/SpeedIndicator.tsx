78abe11c992397b4835182dc86737dd82bdfc930
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-02-20 23:28:57 +0100

    Implement speed margin symbology on PFD

--- src/PFDV2/pfd/components/SpeedIndicator.tsx
+++ src/PFDV2/pfd/components/SpeedIndicator.tsx
@@ -832,8 +832,42 @@ class SpeedTarget extends DisplayComponent <{ bus: EventBus }> {
                 <text ref={this.upperBoundRef} id="SelectedSpeedLowerText" class="FontSmallest EndAlign Cyan" x="24.078989" y="128.27917">{this.textSub}</text>
                 <text ref={this.lowerBoundRef} id="SelectedSpeedLowerText" class="FontSmallest EndAlign Cyan" x="24.113895" y="36.670692">{this.textSub}</text>
                 <path ref={this.speedTargetRef} class="NormalStroke CornerRound Cyan" style="transform: translate3d(0px, 0px, 0px)" d="m19.274 81.895 5.3577 1.9512v-6.0476l-5.3577 1.9512" />
+                <SpeedMargins bus={this.props.bus} />
             </>
         );
+    }
+}
+
+class SpeedMargins extends DisplayComponent<{ bus: EventBus }> {
+    private upperSpeedMarginRef = FSComponent.createRef<SVGPathElement>();
+
+    private lowerSpeedMarginRef = FSComponent.createRef<SVGPathElement>();
+
+    onAfterRender(node: VNode): void {
+        super.onAfterRender(node);
+
+        const sub = this.props.bus.getSubscriber<PFDSimvars>();
+
+        sub.on('upperSpeedMargin').handle(this.moveToSpeed(this.upperSpeedMarginRef));
+
+        sub.on('lowerSpeedMargin').handle(this.moveToSpeed(this.lowerSpeedMarginRef));
+    }
+
+    render(): VNode {
+        return (
+            <g id="SpeedMargins">
+                <path id="UpperSpeedMargin" class="Fill Magenta" d="m19.7 60 h 5.3577 v 0.7 h-5.3577 z" />
+                <path id="LowerSpeedMargin" class="Fill Magenta" d="m19.7 40 h 5.3577 v 0.7 h-5.3577 z" />
+            </g>
+        );
+    }
+
+    private moveToSpeed<T extends(HTMLElement | SVGElement)>(component: NodeReference<T>) {
+        return (speed: number) => {
+            const offset = speed * DistanceSpacing / ValueSpacing;
+
+            component.instance.style.transform = `translate3d(0px, ${offset}px, 0px)`;
+        };
     }
 }
 
