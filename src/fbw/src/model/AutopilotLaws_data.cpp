2540dc60849862fc4d857157b115cbf3da4a1602
Andreas Guther <andreas@guther.family>
2022-04-18 11:08:28 +0200

    feat(ap): improved LOC* to LOC transition and tuned GS law #7094 @2304d0790

--- src/fbw/src/model/AutopilotLaws_data.cpp
+++ src/fbw/src/model/AutopilotLaws_data.cpp
@@ -156,6 +156,12 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
   },
 
 
+  { 100.0, 150.0, 200.0, 250.0, 300.0, 400.0, 500.0 },
+
+
+  { 0.0, 50.0, 100.0, 1000.0, 2500.0, 3000.0 },
+
+
   { 0.0, 150.0, 200.0 },
 
 
@@ -163,12 +169,6 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
 
   { 100.0, 150.0, 200.0, 250.0, 300.0, 400.0, 500.0 },
-
-
-  { 100.0, 150.0, 200.0, 250.0, 300.0, 400.0, 500.0 },
-
-
-  { 0.0, 50.0, 100.0, 1000.0, 2500.0, 3000.0 },
 
 
   { 0.0, 100.0, 200.0, 400.0, 1000.0 },
@@ -179,11 +179,11 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   { 0.0, 100.0, 150.0, 200.0, 250.0, 300.0, 400.0 },
 
+  1.0,
+
+  1.0,
+
   4.0,
-
-  1.0,
-
-  1.0,
 
   2.0,
 
@@ -239,13 +239,17 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   0.33333333333333331,
 
+  10.0,
+
   1.0,
 
   4.0,
 
-  2.0,
+  10.0,
 
-  1.0,
+  10.0,
+
+  0.5,
 
   50.0,
 
@@ -421,7 +425,15 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   0.0,
 
+  0.0,
+
   -3.0,
+
+
+  { 1.6, 1.6, 2.0, 2.8, 3.2, 4.2, 4.5 },
+
+
+  { 3.7, 3.7, 4.3, 7.8, 15.0, 15.0 },
 
 
   { 0.8, 0.2, 0.2 },
@@ -433,13 +445,7 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
   { 1.6, 1.6, 2.0, 2.8, 3.2, 4.2, 4.5 },
 
 
-  { 1.6, 1.6, 2.0, 2.8, 3.2, 4.2, 4.5 },
-
-
-  { 3.7, 3.7, 4.3, 7.8, 15.0, 15.0 },
-
-
-  { 5.0, 5.0, 7.0, 12.0, 12.0 },
+  { 2.0, 2.0, 4.0, 8.0, 8.0 },
 
 
   { 0.0, 0.0, -0.15, -0.4, -0.775, -1.6, -3.0, -3.0 },
@@ -455,13 +461,15 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   4.0,
 
+  5.0,
+
+  30.0,
+
   6.0,
 
   1.0,
 
   2.0,
-
-  30.0,
 
   1.0,
 
@@ -501,6 +509,8 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   0.0,
 
+  -1000.0,
+
   -10.0,
 
   -1000.0,
@@ -510,6 +520,8 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
   -15.0,
 
   -10.0,
+
+  0.5,
 
   10.0,
 
@@ -569,15 +581,21 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   360.0,
 
-  1.1,
-
   0.017453292519943295,
 
   0.2,
 
+  1.1,
+
   15.0,
 
   -15.0,
+
+  1.0,
+
+  0.0,
+
+  1.0,
 
   0.0,
 
@@ -797,6 +815,12 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   1.0,
 
+  10.0,
+
+  360.0,
+
+  0.0,
+
   360.0,
 
   360.0,
@@ -829,8 +853,6 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   360.0,
 
-  360.0,
-
   -1.0,
 
   1.0,
@@ -854,10 +876,6 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
   3.0,
 
   0.8,
-
-  10.0,
-
-  0.0,
 
   -1.0,
 
@@ -1415,7 +1433,17 @@ AutopilotLawsModelClass::Parameters_AutopilotLaws_T AutopilotLawsModelClass::Aut
 
   0.016666666666666666,
 
-  -0.5,
+  0.017453292519943295,
+
+  0.51444444444444448,
+
+  3.2808398950131235,
+
+  -0.25,
+
+  0.8,
+
+  -0.8,
 
   1.0,
 
