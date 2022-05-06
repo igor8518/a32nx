db8c33d4b63f7ce40dc0537733a3885cb8540668
Igor Milovidov <igor8518@mail.ru>
2022-05-06 13:49:40 +0700

    LVarsWrapper: add var

--- src/lvarswrapper/src/main.cpp
+++ src/lvarswrapper/src/main.cpp
@@ -1149,6 +1149,7 @@ __attribute__((export_name("LVarsWrapper_gauge_callback"))) extern "C" bool LVar
                           VarNames.push_back("A32NX_EFIS_L_TO_WPT_IDENT_1");
                           VarNames.push_back("A32NX_EFIS_R_TO_WPT_IDENT_0");
                           VarNames.push_back("A32NX_EFIS_R_TO_WPT_IDENT_1");
+                          VarNames.push_back("A32NX_AUTOBRAKES_ARMED_MODE");
                         }
 
                         hr = SimConnect_CallDispatch(hSimConnect, ProcessVars, 0);
