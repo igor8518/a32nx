768b74fa4ae63523d1b8bb50acd8e7e632343c0f
Saschl <sascharudolf46@gmail.com>
2023-01-04 21:01:25 +0100

    refactor: port SD to full react-based instrument #7345 @2c1fd8e

--- src/instruments/src/EWD/index.tsx
+++ src/instruments/src/EWD/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { DisplayUnit } from '@instruments/common/displayUnit';
 import { render } from '../Common';
 import UpperDisplay from './elements/UpperDisplay';
@@ -6,23 +6,16 @@ import LowerDisplay from './elements/LowerDisplay';
 
 import './style.scss';
 
-export const EWD: React.FC = () => {
-    const [displayIndex] = useState(() => {
-        const url = document.getElementsByTagName('a32nx-ewd')[0].getAttribute('url');
-        return url ? parseInt(url.substring(url.length - 1), 10) : 0;
-    });
-
-    return (
-        <DisplayUnit
-            electricitySimvar="L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED"
-            potentiometerIndex={displayIndex}
-        >
-            <svg className="ewd-svg" version="1.1" viewBox="0 0 768 768" xmlns="http://www.w3.org/2000/svg">
-                <UpperDisplay />
-                <LowerDisplay />
-            </svg>
-        </DisplayUnit>
-    );
-};
+export const EWD: React.FC = () => (
+    <DisplayUnit
+        electricitySimvar="L:A32NX_ELEC_AC_ESS_BUS_IS_POWERED"
+        potentiometerIndex={93}
+    >
+        <svg className="ewd-svg" version="1.1" viewBox="0 0 768 768" xmlns="http://www.w3.org/2000/svg">
+            <UpperDisplay />
+            <LowerDisplay />
+        </svg>
+    </DisplayUnit>
+);
 
 render(<EWD />);
